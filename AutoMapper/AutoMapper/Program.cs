abb221922122da88d8f4499a0da2f4508b4301da
Suganya Panneerselvam <suganya.panneerselvam@dataquest.com.au>
2021-09-30 13:31:12 +0530

    created new method with using automapper

--- AutoMapper/AutoMapper/Program.cs
+++ AutoMapper/AutoMapper/Program.cs
@@ -8,10 +8,33 @@ namespace AutoMapper
 {
     class Program
     {
+        private static IMapper mapper;
         static void Main(string[] args)
         {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AllowNullCollections = true;
+                cfg.CreateMap<Merchandise, MerchandiseView>()
+                .ForMember(x => x.DiffName, y => y.MapFrom(z => z.Name));
+            });
+            mapper = config.CreateMapper();
+            config.AssertConfigurationIsValid();
+
             WithoutAutoMapper();
+            AutoMapperSimpleEx();
         }
+
+        private static void AutoMapperSimpleEx()
+        {
+            var merchandise = new Merchandise();
+            merchandise.ID = 2;
+            merchandise.Name = "Gift Card";
+            merchandise.Price = 50;
+
+            MerchandiseView merchandiseView = mapper.Map<MerchandiseView>(merchandise);
+            Console.WriteLine(merchandiseView.ID);
+        }
+
         public static void WithoutAutoMapper()
         {
             var merchandise = new Merchandise();
